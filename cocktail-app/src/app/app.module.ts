5940ebf21588ce19f5ba8aa2803e14c46cf6d47c
Jeannette Townsend <111323478+jnettownsend@users.noreply.github.com>
2022-11-28 20:15:26 -0600

    Commit with components

--- cocktail-app/src/app/app.module.ts
+++ cocktail-app/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavagationComponent } from './navagation/navagation.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavagationComponent,
+    FavoritesComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
