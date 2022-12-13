6bfcec5aca4fc52afce61fe59c2a65cbd8a9b506
John P <70922864+jpierce-coder@users.noreply.github.com>
2022-12-12 20:20:50 -0600

    Update search.component.ts

--- cocktail-app/src/app/search/search.component.ts
+++ cocktail-app/src/app/search/search.component.ts
@@ -4,21 +4,20 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-
   onFetchCocktails(searchInput: string) {
     const formattedQuery = searchInput.split(' ').join('+').toLowerCase();
     this.http
-      .get(`hhttps://www.thecocktaildb.com/api/json/v1/1/random.php${formattedQuery}`)
+      .get(
+        `https://www.thecocktaildb.com/api/json/v1/1/random.php?=${formattedQuery}`
+      )
       .subscribe((searchResults) => {
         console.log('searchResults', searchResults);
-})
-}
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
+      });
   }
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {}
 }
